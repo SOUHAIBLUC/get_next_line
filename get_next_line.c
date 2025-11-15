735e2d42b4dbd96f6441595211d8b2057cebc877
Souhaib Ait Lahcine <so-ait-l@student.1337.ma>
2025-11-15 18:47:05 +0100

    solved

--- get_next_line.c
+++ get_next_line.c
@@ -12,13 +12,49 @@
 
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+char    *read_line(int fd, char **reminder)
 {
-    static char *reminder;
-    char *line;
+	char *buffer;
+	int bytes_read;
+	char *newline_pos;
+	char *line;
 
-    if(fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-        line = read_line(fd, reminder);
-    if (!line)
-        return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(*reminder, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		if (bytes_read > 0)
+			*reminder = ft_strjoin(*reminder, buffer);
+		if (!*reminder)
+		{
+			free(buffer);
+			return (NULL);
+		}
+	}
+	free(buffer);
+	newline_pos = ft_strchr(*reminder, '\n');
+	*reminder = ft_strdup(newline_pos + 1); // check if newlinepos = "ksajhas\n";
+	if(!*reminder)
+		return (NULL);
+
+char	*get_next_line(int fd)
+{
+	static char *reminder;
+	char *line;
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+		line = read_line(fd, &reminder);
+	if (!line)
+		return (NULL);
+	return (line);
+}
